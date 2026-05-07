bb0d3fc3468fed5766749e2074cccb973b4f0ebe
bstanojevik91 <bstanojevik91@gmail.com>
2026-05-07 17:43:05 +0200

    Add Google tag to shared layouts

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="mk">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VDST4H1ZZ7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-VDST4H1ZZ7');
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Admin | Honorarec.mk' }}</title>
