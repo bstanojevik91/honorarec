114c10e93899dbd74f9da1cebff7ce60c9dc0701
bstanojevik91 <bstanojevik91@gmail.com>
2026-07-27 22:19:29 +0200

    Update privacy controller country label

--- config/privacy.php
+++ config/privacy.php
@@ -3,7 +3,7 @@
 return [
     'controller_name' => 'Бојан Станојевиќ',
     'controller_description' => 'физичко лице и оператор на платформата Honorarec.mk',
-    'controller_country' => 'Република Северна Македонија',
+    'controller_country' => 'Република Македонија',
     'contact_email' => 'info@honorarec.mk',
     'policy_version' => '2026-07-27',
     'last_updated' => '27.07.2026',
